7c20a4a40ac1f8bb64807f95c8964890e40420b1
shazvan275 <shazvanhanif@gmail.com>
2017-09-28 17:48:39 +0400

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Http\Controllers\Api;
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\JWTAuth;
+class AuthController extends Controller
+{
+	private $user;
+	private $jwtauth;
+	public function __construct(User $user, JWTAuth $jwtauth)
+	{
+	  $this->user = $user;
+	  $this->jwtauth = $jwtauth;
+	}
+  public function register(Request $request)
+  {
+    $newUser = $this->user->create([
+      'name' => $request->get('name'),
+      'email' => $request->get('email'),
+      'password' => bcrypt($request->get('password'))
+    ]);
+    if (!$newUser) {
+      return response()->json(['failed_to_create_new_user'], 500);
+    }
+    return response()->json([
+		'token' => $this->jwtauth->fromUser($newUser)
+	]);
+  }
+  public function login(Request $request)
+	{
+	  // get user credentials: email, password
+	  $credentials = $request->only('email', 'password');
+	  $token = null;
+	  try {
+		$token = $this->jwtauth->attempt($credentials);
+		if (!$token) {
+		  return response()->json(['invalid_email_or_password'], 422);
+		}
+	  } catch (JWTAuthException $e) {
+		return response()->json(['failed_to_create_token'], 500);
+	  }
+	  return response()->json(compact('token'));
+	}
+}
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Requests;
+use App\Http\Requests\Request;
+class LoginRequest extends Request
+{
+
+  public function authorize()
+  {
+    return true;
+  }
+
+  public function rules()
+  {
+    return [
+      'email' => 'required',
+      'password' => 'required'
+    ];
+  }
+}
